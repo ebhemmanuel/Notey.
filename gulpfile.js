c0ae937d3f9c6f631512c1d264a17edcc45478d6
Emma <its@justbarreto.com>
2017-10-11 23:29:06 -0400

    SASS Implementation Done
    Added all the right paths and variables for the properties.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
-var gulp = require('gulp'),
-	 concat = require('gulp-concat'),
-	 sass = require('gulp-sass'),
-	 uglify = require('gulp-uglify'),
-	 cleanCSS = require('gulp-clean-css');
+var gulp  		 = require('gulp'),
+	 concat 		 = require('gulp-concat'),
+	 sass 		 = require('gulp-sass'),
+	 uglify      = require('gulp-uglify'),
+	 cleanCSS    = require('gulp-clean-css');
+
+var jsFiles     = './js/**/*.js'
+	 jsAppFile   = './js/app.js',
+	 jsAppOutput = './myApp.js',
+	 jsDest 		 = './public/js',
+	 sassFiles   = './build/sass/**/*.sass',
+	 sassDest    = './public/css/';
+
 gulp.task('js:build', function() {
-	return gulp.src(['./js/app.js', './js/**/*.js'])
-	.pipe(concat('./myApp.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('./public/js'))
+	return gulp.src([jsAppFile, jsFiles])
+	.pipe(concat(jsAppOutput))
+	// .pipe(uglify())
+	.pipe(gulp.dest(jsDest))
 })
-gulp.task('js:watch', ['js:build'], function() {
-	gulp.watch('./js/**/*.js', ['js:build'])
+
+gulp.task('sass:build', function() {
+	return gulp.src(sassFiles)
+	.pipe(sass())
+	.pipe(cleanCSS())
+	.pipe(gulp.dest(sassDest));
 })
-gulp.task('sass', function() {
-	return sass('./build/sass/**/*.sass', {style:'expanded'})
-	.pipe(css())
-	.pipe(gulp.dest('./public/css/main.css'));
+gulp.task('watch', ['js:build','sass:build'], function() {
+	gulp.watch(jsFiles, ['js:build'])
+	gulp.watch(sassFiles, ['sass:build'])
 })
 gulp.task('default', ['js:watch'])
